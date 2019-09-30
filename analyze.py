e759edbf1d03ea417fbab4a57dc1e6440cb5a141
Hemmendinger <github@hemmendinger.net>
2019-09-30 15:22:59 -0400

    api setup func

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import tweepy
+import os
+
+
+def setup_api():
+    auth = tweepy.OAuthHandler(os.getenv('TWITTER_CONSUMER_KEY'), os.getenv('TWITTER_CONSUMER_SECRET'))
+    auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_SECRET'))
+    api = tweepy.API(auth)
+
+    return api
