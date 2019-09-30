56cd62e8b8ed6c09e72279795f59582dc9e4069a
Hemmendinger <github@hemmendinger.net>
2019-09-30 16:58:12 -0400

    override api defaults for rate limits and retries

--- analyze.py
+++ analyze.py
@@ -5,6 +5,11 @@ import os
 def setup_api():
     auth = tweepy.OAuthHandler(os.getenv('TWITTER_CONSUMER_KEY'), os.getenv('TWITTER_CONSUMER_SECRET'))
     auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_SECRET'))
-    api = tweepy.API(auth)
+    api = tweepy.API(auth,
+                     wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True,
+                     retry_count=3,
+                     retry_delay=3,
+                     )
 
     return api
