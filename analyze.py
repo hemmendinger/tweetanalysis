003f8142171d33006f29213d245334b14e084951
Hemmendinger <github@hemmendinger.net>
2019-09-30 17:03:05 -0400

    started process tweet func

--- analyze.py
+++ analyze.py
@@ -13,3 +13,23 @@ def setup_api():
                      )
 
     return api
+
+
+def process_tweet_object(tweet):
+    output = dict()
+
+    output['sceen_name'] = tweet.screen_name
+    output['id'] = tweet.id_str
+    output['created_at'] = tweet.created_at  # UTC time
+    output['text'] = tweet.text.encode("utf-8")
+    output['truncated'] = tweet.truncated
+    output['source'] = tweet.source,  # client used to send
+
+    # tweet.coordinates,
+    # tweet.place
+    output['in_reply_to_screen_name'] = tweet.in_reply_to_screen_name
+    output['in_reply_to_status_id_str'] = tweet.in_reply_to_status_id_str
+    output['in_reply_user_id_str'] = tweet.in_reply_to_user_id_str
+
+    if hasattr(tweet, 'quoted_status_id_str'):
+        output['quoted_status_id_str'] = tweet.quoted_status_id_str
